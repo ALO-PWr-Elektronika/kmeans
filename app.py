1582ab5c7dcc459404facad5e7353878c6a73374
Rafał <rafal.piotr.wlodarczyk@gmail.com>
2024-04-09 15:53:58 +0200

    kmeans template

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, render_template
 import numpy as np
 
 from kmeans import KMeans
@@ -21,6 +21,22 @@ def hello_world():
     }
     
     return jsonify(result)
+
+
+@app.route("/kmeans")
+def hello():
+    points = np.array([(1, 2), (3, 4), (5, 6), (7, 8)])
+    k = 2
+    centroids, assigments = KMeans.kmeans(points,k)
+    
+    result = {
+        'points': points,
+        'k': k,
+        'centroids': centroids.tolist(),
+        'assignments': assigments.tolist()
+    }
+
+    return render_template('kmeans.html', result=result)
     
 
 if __name__ == "__main__":
